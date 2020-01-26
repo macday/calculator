1e9866bdb4da941d863b31825d1762743f596efa
macday <mckenziewjday@gmail.com>
2020-01-25 22:50:24 -0500

    Changed styling, needs input request

--- src/Calculator.js
+++ src/Calculator.js
@@ -5,23 +5,37 @@ function Calculator() {
   return (
     <div className="Calculator">
       <div class='title'>
-          Calculator App
+        Calculator App
       </div>
       <div class='calculator'>
-          <div class='result'>
-            <text id='output'></text>
-          </div>
-          <div class='keypad'>
-            <button class='number'>1</button>
-            <button class='number'>2</button>
-            <button class='number'>3</button>
-            <button class='number'>4</button>
-            <button class='number'>5</button>
-            <button class='number'>6</button>
-            <button class='number'>7</button>
-            <button class='number'>8</button>
-            <button class='number'>9</button>
-          </div>
+        <div class='result'>
+          <text id='output'>0</text>
+        </div>
+        <div class='keypad'>
+          <button class='operand'>C</button>
+          <button class='operand'>+/-</button>
+          <button class='operand'>%</button>
+          <button class='operation'>/</button>
+
+          <button class='number'>7</button>
+          <button class='number'>8</button>
+          <button class='number'>9</button>
+          <button class='operation'>x</button>
+
+          <button class='number'>4</button>
+          <button class='number'>5</button>
+          <button class='number'>6</button>
+          <button class='operation'>-</button>
+
+          <button class='number'>1</button>
+          <button class='number'>2</button>
+          <button class='number'>3</button>
+          <button class='operation'>+</button>
+
+          <button class='zero number'>0</button>
+          <button class='number'>.</button>
+          <button class='equals operation'>=</button>
+        </div>
       </div>
     </div>
   );
