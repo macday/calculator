d6d67b2b06abc3720bf3b2088ce55e083e66765e
macday <mckenziewjday@gmail.com>
2020-01-26 20:42:27 -0500

    Final styling, clock style change, and logic functionality added

--- src/Calculator.js
+++ src/Calculator.js
@@ -3,6 +3,40 @@ import './Calculator.css';
 import './Clock.css';
 
 function Calculator() {
+  let displayed = false;
+  function calc(input) {
+    var operators = ['*', '/', '+', '-', '%'];
+    if (displayed == true && !operators.includes(input)) {
+      document.getElementById('output').innerHTML = '0';
+    }
+    displayed = false;
+    var current = document.getElementById('output').innerHTML;
+    var output = '0';
+    console.log(current);
+    if (!isNaN(input) || operators.includes(input) || input === '.') {
+      if (output != current) {
+        output = current + input;
+      } else if (!operators.includes(input)){
+        output = input;
+      }
+    } else if (input === '=') {
+      try {
+        output = Math.round((eval(current) +Number.EPSILON) * 10000000000) / 10000000000;
+        output = output.toString();
+        displayed = true;
+      } catch (SyntaxError) {
+        alert("Invalid input! Please be careful!");
+      }
+    } else if (input === '+/-') {
+      if (current[0] == '-') {
+        console.log('neg');
+      } else {
+        console.log('pos');
+      }
+    }
+    document.getElementById('output').innerHTML = output;
+  }
+
   return (
     <div className="Calculator">
       <body>
@@ -11,32 +45,32 @@ function Calculator() {
         </div>
         <div class='calculator'>
           <div class='result'>
-            <text id='output'>0</text>
+            <div id='output'>0</div>
           </div>
           <div class='keypad'>
-            <button class='operand'>C</button>
-            <button class='operand'>+/-</button>
-            <button class='operand'>%</button>
-            <button class='operation'>/</button>
+            <button onClick={() => calc('C')} class='btn operand'>C</button>
+            <button onClick={() => calc('+/-')} class='btn operand'>+/-</button>
+            <button onClick={() => calc('%')} class='btn operand'>%</button>
+            <button onClick={() => calc('/')} class='btn operation'>/</button>
 
-            <button class='number'>7</button>
-            <button class='number'>8</button>
-            <button class='number'>9</button>
-            <button class='operation'>x</button>
+            <button onClick={() => calc(7)} class='btn number'>7</button>
+            <button onClick={() => calc(8)} class='btn number'>8</button>
+            <button onClick={() => calc(9)} class='btn number'>9</button>
+            <button onClick={() => calc('*')} class='btn operation'>x</button>
 
-            <button class='number'>4</button>
-            <button class='number'>5</button>
-            <button class='number'>6</button>
-            <button class='operation'>-</button>
+            <button onClick={() => calc('4')} class='btn number'>4</button>
+            <button onClick={() => calc(5)} class='btn number'>5</button>
+            <button onClick={() => calc(6)} class='btn number'>6</button>
+            <button onClick={() => calc('-')} class='btn operation'>-</button>
 
-            <button class='number'>1</button>
-            <button class='number'>2</button>
-            <button class='number'>3</button>
-            <button class='operation'>+</button>
+            <button onClick={() => calc(1)} class='btn number'>1</button>
+            <button onClick={() => calc(2)} class='btn number'>2</button>
+            <button onClick={() => calc(3)} class='btn number'>3</button>
+            <button onClick={() => calc('+')} class='btn operation'>+</button>
 
-            <button class='zero number'>0</button>
-            <button class='number'>.</button>
-            <button class='equals operation'>=</button>
+            <button onClick={() => calc(0)} class='btn zero number'>0</button>
+            <button onClick={() => calc('.')} class='btn number'>.</button>
+            <button onClick={() => calc('=')} class='btn equals operation'>=</button>
           </div>
         </div>
 
@@ -49,10 +83,12 @@ function Calculator() {
 
 function getTime() {
   var date = new Date();
-  var hour = date.getHours();
-  var minute = date.getMinutes();
-  var second = date.getSeconds();
-  document.getElementById('clock').innerHTML = hour%12+":"+minute+":"+second;
+  document.getElementById('clock').innerHTML = date.toLocaleTimeString('en-US', {
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit',
+    hour12: true
+  });
 }
 setInterval(getTime, 1000)
 
